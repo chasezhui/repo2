5b3fbd197036638cbed147a0f46b717cfaea8eec
tom_glb <goodmorning_glb@atguigu.com>
2020-05-05 15:42:54 +0800

    添加

---
+++ untitled1/untitled/src/cn/itcast/domain/Test.java
@@ -0,0 +1,19 @@
+package cn.itcast.domain;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试测试.....");
+        System.out.println("测试测试.....");
+        System.out.println("测试测试.....");
+        System.out.println("测试测试.....");
+        System.out.println("测试测试.....");
+        System.out.println("测试测试.....");
+        System.out.println("测试测试.....");
+        System.out.println("测试测试.....");
+        System.out.println("测试测试.....");
+        System.out.println("测试测试.....");
+        System.out.println("测试测试.....");
+        System.out.println("测试测试.....");
+
+    }
+}
